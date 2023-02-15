2122465366b5687d370bdfb8aac14b938cb9a257
sendrastisch <sandra.fernandes@student.hu.nl>
2023-02-15 16:30:15 +0100

    add first commit with navbar

---
+++ majestic-meridian/src/components/navbar.jsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+
+const Navbar = ({  }) => {
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(true);
+        console.log("hey?")
+    };
+
+    const onLeave = () => {
+        setHover(false);
+    };
+
+
+    function handleClick(){
+        console.log(classTail)
+    }
+
+    return (
+        <div className="shadow-2xl bg-black flex justify-between border-b-2 ">
+            <p className="text-orange m-7">Sandra Fernandes</p>
+            <ul className="flex flex-row space-x-9 m-7">
+                <li className="cursor-pointer">
+                    <a className="flex text-white transition ease-in-out delay-40 hover:text-orange"><p className="mr-1.5 text-orange">01.</p>About</a>
+                </li>
+                <li className="cursor-pointer">
+                    <a className="flex text-white transition ease-in-out delay-40 hover:text-orange"><p className="mr-1.5 text-orange">02. </p>Skills</a>
+                </li>
+                <li className="cursor-pointer">
+                    <a className="flex text-white transition ease-in-out delay-40 hover:text-orange"><p className="mr-1.5 text-orange">03. </p>Projects</a>
+                </li>
+                <li className="cursor-pointer">
+                    <a className="flex text-white transition ease-in-out delay-40 hover:text-orange "><p className="mr-1.5 text-orange">04. </p>Contact</a>
+                </li>
+            </ul>
+        </div>
+
+    )
+}
+
+export default Navbar;
---
+++ majestic-meridian/tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		colors: {
+			'black': '#1A1C20',
+			'orange': '#CF7500',
+			'white': '#D3D3D3'
+		}
+	},
+	plugins: [],
+}
